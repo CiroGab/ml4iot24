0c3138c07db9adf6e60ada3164ac99c7d205751d
Gabriele Cirotto <gabrielecirotto@gmail.com>
2023-11-20 17:11:12 +0100

    Update EX2_1.py
    Changed timeseries name in plugged_seconds

--- EX2_1.py
+++ EX2_1.py
@@ -5,8 +5,7 @@ import uuid
 import argparse
 
 if __name__ == "__main__":
-
-
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type = str)
     parser.add_argument("--port", type=str)
@@ -38,24 +37,11 @@ if __name__ == "__main__":
         pass
 
     try:
-        redis_client.ts().create(f'{mac_address}:plugged_seconds_sum', retention_msecs= 30*24*60*60*1000) #Retention of one month
-        redis_client.ts().createrule(f'{mac_address}:power', f'{mac_address}:plugged_seconds_sum', 'sum', bucket_size_msec=60*60*1000) #Computed every hour
+        redis_client.ts().create(f'{mac_address}:plugged_seconds', retention_msecs= 30*24*60*60*1000) #Retention of one month
+        redis_client.ts().createrule(f'{mac_address}:power', f'{mac_address}:plugged_seconds', 'sum', bucket_size_msec=60*60*1000) #Computed every hour
     except redis.ResponseError:
         pass
-
-
-    print("--- Maximum number of clients (a client is defined by its mac-address) ---")
-    print("Memory for battery: 16 Bytes per record (8 timestamp, 8 value)")
-    print("Memory for power: 16 Bytes per record (8 timestamp, 8 value)")
-    print("Memory for plugged time: 16 Bytes per record (8 timestamp, 8 value)")
-    print("Max number of battery records per client: 60*60*24 = 86400 (Retention of one day, one record per second)")
-    print("Max number of power records per client: 60*60*24 = 86400 (Retention of one day, one record per second)")
-    print("Max number of plugged time records per client: 24*30 = 720 (Retention of one month, one record per hour)")
-    print("Max number of records: 173520")
-    print("Max number of bytes (without header): 2776320 Bytes (2.65 MB)")
-    print("Max number of bytes (without header) after compression: 277632 Bytes (0.26 MB)")
-    print("Max memory usage: 104857600 Bytes (100 MB)")
-    print("Max number of clients: 104857600 Bytes / 277632 Bytes = 377.6 Clients (377 Clients at full capacity)")
+        
     
     while True:
         timestamp = time.time()
