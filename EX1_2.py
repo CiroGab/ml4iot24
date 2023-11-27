b54224f8b7e98edccbe3f4d031d9695f5a77d650
anubis09 <lorenzotozzi98@gmail.com>
2023-11-27 12:59:03 +0100

    type hinting and comments added

--- EX1_2.py
+++ EX1_2.py
@@ -3,6 +3,7 @@
 import tensorflow as tf
 import argparse
 import numpy as np
+import numpy.typing as npt
 import os
 import sounddevice as sd
 from time import time
@@ -15,7 +16,8 @@ BLOCKSIZE = int(0.5 * SAMPLING_RATE)
 audio_buffer = np.zeros(shape=(SAMPLING_RATE, CHANNELS))
 
 
-def preprocess_audio(indata):
+def preprocess_audio(indata: npt.ArrayLike) -> tf.Tensor:
+    """Return the preprocessed audio."""
     tf_indata = tf.convert_to_tensor(indata, dtype=tf.float32)
     audio_tensor = tf.squeeze(tf_indata)
     audio_normalized = audio_tensor / tf.int16.max
@@ -24,11 +26,18 @@ def preprocess_audio(indata):
 
 
 class Spectrogram:
-    def __init__(self, sampling_rate, frame_length_in_s, frame_step_in_s):
+    def __init__(
+        self,
+        sampling_rate: int,
+        frame_length_in_s: float,
+        frame_step_in_s: float,
+    ):
+        """Initialize frame lenght and frame step."""
         self.frame_length = int(frame_length_in_s * sampling_rate)
         self.frame_step = int(frame_step_in_s * sampling_rate)
 
-    def get_spectrogram(self, audio):
+    def get_spectrogram(self, audio: tf.Tensor) -> tf.Tensor:
+        """Return the spectogram of a given audio."""
         stft = tf.signal.stft(
             audio,
             frame_length=self.frame_length,
@@ -43,13 +52,14 @@ class Spectrogram:
 class MelSpectrogram:
     def __init__(
         self,
-        sampling_rate,
-        frame_length_in_s,
-        frame_step_in_s,
-        num_mel_bins,
-        lower_frequency,
-        upper_frequency,
+        sampling_rate: int,
+        frame_length_in_s: float,
+        frame_step_in_s: float,
+        num_mel_bins: int,
+        lower_frequency: int,
+        upper_frequency: int,
     ):
+        """Initialize spectogram processor and the linear_to_mel_matrix."""
         self.spectrogram_processor = Spectrogram(
             sampling_rate, frame_length_in_s, frame_step_in_s
         )
@@ -65,7 +75,8 @@ class MelSpectrogram:
             )
         )
 
-    def get_mel_spec(self, audio):
+    def get_mel_spec(self, audio: tf.Tensor) -> tf.Tensor:
+        """Return the mel spectogram."""
         spectrogram = self.spectrogram_processor.get_spectrogram(audio)
         mel_spectrogram = tf.matmul(
             spectrogram, self.linear_to_mel_weight_matrix
@@ -78,14 +89,15 @@ class MelSpectrogram:
 class VAD:
     def __init__(
         self,
-        sampling_rate,
-        frame_length_in_s,
-        num_mel_bins,
-        lower_frequency,
-        upper_frequency,
-        dbFSthres,
-        duration_thres,
+        sampling_rate: int,
+        frame_length_in_s: float,
+        num_mel_bins: int,
+        lower_frequency: int,
+        upper_frequency: int,
+        dbFSthres: int,
+        duration_thres: float,
     ):
+        """Initialize the mel spectogram processor and the two thresholds."""
         self.frame_length_in_s = frame_length_in_s
         self.mel_spec_processor = MelSpectrogram(
             sampling_rate,
@@ -98,7 +110,8 @@ class VAD:
         self.dbFSthres = dbFSthres
         self.duration_thres = duration_thres
 
-    def is_silence(self, audio):
+    def is_silence(self, audio: npt.ArrayLike) -> bool:
+        """Return if the audio is silent or not."""
         audio = preprocess_audio(indata=audio)
         log_mel_spec = self.mel_spec_processor.get_mel_spec(audio)
         dbFS = 20 * log_mel_spec
@@ -111,23 +124,23 @@ class VAD:
         non_silence_duration = (non_silence_frames + 1) * self.frame_length_in_s
 
         if non_silence_duration > self.duration_thres:
-            return 0
+            return False
         else:
-            return 1
+            return True
 
 
-def callback(indata, frames, callback_time, status):
+def callback(indata: npt.ArrayLike, frames, callback_time, status):
     """This is called (from a separate thread) for each audio block."""
     global is_audio_buffer_silent, audio_buffer
 
+    # Put the last 0.5s at the beginning of the buffer and the latest at the end.
     audio_buffer = np.roll(audio_buffer, -BLOCKSIZE)
     audio_buffer[BLOCKSIZE:, :] = indata
 
-    is_audio_buffer_silent = bool(
-        voice_activity_detector.is_silence(audio_buffer)
-    )
+    is_audio_buffer_silent = voice_activity_detector.is_silence(audio_buffer)
 
     if not is_audio_buffer_silent:
+        # audio not silent, so we store it.
         timestamp = time()
         write(f"{timestamp}.wav", SAMPLING_RATE, audio_buffer)
         filesize_in_bytes = os.path.getsize(f"{timestamp}.wav")
@@ -140,6 +153,7 @@ if __name__ == "__main__":
     parser.add_argument("--device", type=int, default=0)
     args = parser.parse_args()
     is_audio_buffer_silent = True
+    # Instance of VAD with the parameters found in exercise 1.1
     voice_activity_detector = VAD(
         sampling_rate=16000,
         frame_length_in_s=0.032,
